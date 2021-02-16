6e6f958002caf1ea96bc5ade113c384925c37776
CFE <hello@teamcfe.com>
2021-02-16 12:38:36 -0800

    Python API & Html Documents

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
+from dataclasses import dataclass # pip install dataclasses
 import webview # pip install pywebview
 
-html_str = """
-<h1>Hello World </h1>
-"""
-webview.create_window("PyDesktop", html=html_str)
+
+@dataclass
+class API:
+    name: str
+
+    def helloWorld(self):
+        """
+        pywebview.api.helloWorld()
+        """
+        if not self.name:
+            return "Hello world"
+        return f"Hello world {self.name}"
+
+js_api = API(name='Justin')
+
+window_args = {
+    "js_api": js_api,
+    "width": 1200
+}
+webview.create_window("PyDesktop", "templates/base.html", **window_args)
 webview.start(debug=True)
